460e416cdc4dc3640116395be7d11e86e886510f
Gaizka <gdiaz@nalej.com>
2019-06-04 16:42:04 +0200

    vpnserver compiled in startup.sh

--- components/vpn-server/dockerenv/startup.sh
+++ components/vpn-server/dockerenv/startup.sh
@@ -2,8 +2,14 @@
 
 export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
 
-echo "Copying config..."
-cp /vpn-server/vpn_server.config /usr/vpnserver/vpn_server.config
+# Install VPN Server IF it's not yet installed
+if [ ! -d "$DIRECTORY" ]; then
+    wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.29-9680-rtm/softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-x64-64bit.tar.gz
+    tar -xvzf softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-x64-64bit.tar.gz
+    rm softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-x64-64bit.tar.gz
+    cd vpnserver
+    make
+fi
 
 echo "Starting VPN server..."
 vpnserver start
