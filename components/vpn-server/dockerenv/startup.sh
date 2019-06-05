6a92d2b78954dd9c798cd1b16c027fbf951d3110
Gaizka <gdiaz@nalej.com>
2019-06-05 17:36:12 +0200

    added comment to check if it's the first time launching pod

--- components/vpn-server/dockerenv/startup.sh
+++ components/vpn-server/dockerenv/startup.sh
@@ -2,11 +2,13 @@
 
 export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
 
-# Install VPN Server IF it's not yet installed
+# Stop old vpn server
 vpnserver stop
 
+# Install VPN Server IF it's not yet installed
 CONFIG=/vpnserver/vpn_server.config
 if [ ! -f "$CONFIG" ]; then
+    echo "First time launching VPN server"
     cd /
     cp -r /usr/vpnserver /
     cp /vpn_server.config /vpnserver/vpn_server.config
