bdc4818ca64c31a4aa9aa57ede27306631ac5773
Carmen de Lope Contreras <carmendelope@telefonica.net>
2019-11-08 12:21:54 +0100

    NP-2138 open sourcing

--- internal/pkg/server/vpn/vpn-helper.go
+++ internal/pkg/server/vpn/vpn-helper.go
@@ -1,5 +1,18 @@
 /*
- * Copyright (C) 2019 Nalej - All Rights Reserved
+ * Copyright 2019 Nalej
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 package vpn
@@ -11,17 +24,16 @@ import (
 )
 
 type vpnHelper struct {
-
 }
 
-func NewVPNHelper () *vpnHelper {
+func NewVPNHelper() *vpnHelper {
 	return &vpnHelper{}
 }
 
 // CreateDefaultUser creates a user and password by default for the EIP to connect to it
-func (helper * vpnHelper) CreateDefaultUser () derrors.Error {
+func (helper *vpnHelper) CreateDefaultUser() derrors.Error {
 	//Create user
-	cmd := exec.Command(command, cmdMode, defaultVPNServer, hub, cmdCmd,  userCreateCmd, defaultVPNUser, group, realName, note)
+	cmd := exec.Command(command, cmdMode, defaultVPNServer, hub, cmdCmd, userCreateCmd, defaultVPNUser, group, realName, note)
 	log.Debug().Str("Server", defaultVPNServer).Str("Username", defaultVPNUser).Msg("Default user created in VPN Server")
 
 	err := cmd.Run()
